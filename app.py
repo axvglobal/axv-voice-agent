922721d97dfb35054667aab3979de8f1d28b088f
avanzianet <avanzia.net@gmail.com>
2026-06-05 09:59:53 -0500

    Add Twilio media stream endpoint

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, Response
-from twilio.twiml.voice_response import VoiceResponse
+from flask_sock import Sock
+from twilio.twiml.voice_response import VoiceResponse, Connect
 
 app = Flask(__name__)
+sock = Sock(app)
 
 @app.route("/", methods=["GET"])
 def home():
@@ -9,24 +11,32 @@ def home():
 
 @app.route("/twilio/voice", methods=["POST", "GET"])
 def twilio_voice():
+
     response = VoiceResponse()
 
-    response.say(
-        "Hello, thank you for calling AXV Global. "
-        "This is Alex, your virtual assistant. "
-        "We are currently testing our voice system.",
-        voice="alice",
-        language="en-US"
+    connect = Connect()
+
+    connect.stream(
+        url="wss://axv-voice-agent.onrender.com/media-stream"
     )
 
-    response.pause(length=1)
-
-    response.say(
-        "Please call again later. Thank you.",
-        voice="alice",
-        language="en-US"
-    )
-
-    response.hangup()
+    response.append(connect)
 
     return Response(str(response), mimetype="text/xml")
+
+
+@sock.route("/media-stream")
+def media_stream(ws):
+
+    print("Client connected to media stream")
+
+    while True:
+
+        data = ws.receive()
+
+        if data is None:
+            break
+
+        print(data)
+
+    print("Client disconnected")
